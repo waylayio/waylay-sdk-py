f2e9b1487054d3cfd3f855d454039aff598bc8f9
simon-schoonjans <simon.schoonjans@waylay.io>
2023-12-28 11:51:31 +0100

    chore: clean base api client generated by openapi generator

--- waylay/service/api_response.py
+++ waylay/service/api_response.py
@@ -2,20 +2,22 @@
 
 from __future__ import annotations
 from typing import Any, Dict, Optional, Generic, TypeVar
-from pydantic import Field, StrictInt, StrictStr, StrictBytes, BaseModel
+from dataclasses import dataclass, field
 
 T = TypeVar("T")
 
-class ApiResponse(BaseModel, Generic[T]):
+@dataclass
+class ApiResponse(Generic[T]):
     """
     API response object
     """
 
-    status_code: StrictInt = Field(description="HTTP status code")
-    headers: Optional[Dict[StrictStr, StrictStr]] = Field(None, description="HTTP headers")
-    data: T = Field(description="Deserialized data given the data type")
-    raw_data: StrictBytes = Field(description="Raw data (HTTP response body)")
+    status_code: int
+    """HTTP status code"""
+    data: T
+    """ Deserialized data given the data type """
+    raw_data: bytes
+    """Raw data (HTTP response body)"""
+    headers: Optional[Dict[str, str]] = field(default_factory=dict)
+    """HTTP headers"""
 
-    model_config = {
-        "arbitrary_types_allowed": True
-    }
